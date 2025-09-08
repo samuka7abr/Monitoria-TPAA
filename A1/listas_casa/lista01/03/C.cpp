9e9ee38c9d56bf621dc87c83f99f6f749c24bd5e
samuka7abr <samuelabrao2006@gmail.com>
2025-09-08 20:40:47 -0300

    LEE 03

--- A1/listas_casa/lista01/03/C.cpp
+++ A1/listas_casa/lista01/03/C.cpp
@@ -10,8 +10,26 @@ const int INF_INT = 1e9;
 const ll INF_LL = 1e18; 
 const int MOD = 1000000007;
 
+string convertBase(ull n, int base){
+    if(base == 10) return to_string(n);
+    if(n == 0) return "0";
+    static const string dig = "0123456789ABCDEF";
+    string s;
+    while(n > 0){
+        s.push_back(dig[n % base]);
+        n /= base;
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
 int main(){
     otim;
-    
+    long long N; int B;
+    while (cin >> N >> B){
+        if (N == -1 && B == -1) break;
+        cout << convertBase((ull)N, B) << "\n";
+    }
     return 0;
 }
+
