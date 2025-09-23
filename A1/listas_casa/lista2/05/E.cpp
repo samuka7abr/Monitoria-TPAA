15692f8855196f8a907b8c3607cf255a58179344
samuka7abr <samuelabrao2006@gmail.com>
2025-09-23 10:35:25 -0300

    LEE: 5

--- A1/listas_casa/lista2/05/E.cpp
+++ A1/listas_casa/lista2/05/E.cpp
@@ -2,8 +2,37 @@
 using namespace std;
 #define otim ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
+bool ehPrimo(int x) {
+	if (x < 2) return false;
+	for (int i = 2; i <= sqrt(x); i++) {
+		if (x % i == 0)
+		return false;
+    	}
+    	return true;
+}
+
 int main(){
 	otim;
-	
+	int N; cin >> N;
+
+	for(int i = 0; i < N; i ++){
+		long long num; cin >> num;
+
+		long long temp = num;
+
+		int count = 0;
+
+		while(temp){
+			count += (temp & 1);
+			temp >>= 1;
+		}
+
+		if(ehPrimo(count)){
+			cout << num << " eh um pribit\n";
+		}else{
+			cout << num << " nao eh um pribit\n";
+		}
+		
+	}
 	return 0;
 }
