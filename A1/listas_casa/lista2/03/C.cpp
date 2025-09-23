532118fcc067d056474cd2831aef0e3821b8959f
samuka7abr <samuelabrao2006@gmail.com>
2025-09-23 10:11:40 -0300

    LEE: 3

--- A1/listas_casa/lista2/03/C.cpp
+++ A1/listas_casa/lista2/03/C.cpp
@@ -3,7 +3,42 @@ using namespace std;
 #define otim ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 int main(){
-	otim;
-	
-	return 0;
+    otim;
+    int T; 
+    if(!(cin >> T)) return 0;
+    while(T--){
+        int N; 
+        cin >> N;
+
+        vector<long long> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+
+        long long S; 
+        cin >> S;
+
+        int left = 0, right = N - 1;
+        bool found = false;
+
+        while(left < right) {
+            long long somaAtual = arr[left] + arr[right];
+            if(somaAtual == S){
+                found = true;
+                break;
+            } else if(somaAtual < S){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        if(found){
+            cout << "SIM\n";
+        } else {
+            cout << "NAO\n";
+        }
+    }
+    return 0;
 }
+
