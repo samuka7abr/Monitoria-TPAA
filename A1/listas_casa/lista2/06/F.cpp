7ffe2bd45ea9f224ddd055dd9f0057aac54817e5
samuka7abr <samuelabrao2006@gmail.com>
2025-09-23 10:37:15 -0300

    LEE: 2:6

--- A1/listas_casa/lista2/06/F.cpp
+++ A1/listas_casa/lista2/06/F.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define otim ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 int main(){
-	otim;
-	
-	return 0;
+    int T;
+    cin >> T;
+
+    for(int i = 0 ; i < T ; i++){
+        long long A, B;
+        long long Y;
+        cin >> A >> B;
+        cin >> Y;
+
+        long long D = A | B;
+
+        if((D & (~Y)) != 0){
+            cout << -1 << endl;
+        }else{
+            long long C = Y ^ D;
+            cout << C << endl;
+        }
+    }
+    
+
+    return 0;
 }
