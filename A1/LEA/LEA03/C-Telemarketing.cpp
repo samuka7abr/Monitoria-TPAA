fcefc868ffbb7ee2bae07ed67dfa0788c65bdac8
Falizz <fabioluisdecarvalhoterra@gmail.com>
2025-09-29 18:40:59 -0300

    questao C LEA03

--- A1/LEA/LEA03/C-Telemarketing.cpp
+++ A1/LEA/LEA03/C-Telemarketing.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define otim ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+#define otim ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+/*
+    Ideia de resolução:
+    - Utilizar um deque para representar uma janela deslizante de tempo das chamadas.
+    - Para cada chamada, adiciona o tempo ao deque.
+    - Remove do início do deque todos os tempos que estão fora da janela de tamanho K.
+    - O tamanho do deque após as remoções indica quantas chamadas estão dentro da janela.
+*/
 
 int main(){
-	otim
-	ll C, K;
-	cin >> C >> K;
+    otim
+    ll C, K;
+    cin >> C >> K;
 
-	vector<ll> T(C);
+    vector<ll> T(C);
 
-	for(ll i = 0; i < C; i++) cin >> T[i];
+    for(ll i = 0; i < C; i++) cin >> T[i];
 
-	deque<ll> dq;
+    deque<ll> dq;
 
-	for(ll i = 0; i < C; i++){
-		dq.push_back(T[i]);
+    for(ll i = 0; i < C; i++){
+        dq.push_back(T[i]);
 
-		ll limite = T[i] - K;
+        ll limite = T[i] - K;
 
-		while(!dq.empty() &&  dq.front() < limite){
-			dq.pop_front();
-		}
+        while(!dq.empty() && dq.front() < limite){
+            dq.pop_front();
+        }
 
-		if(i){
-			cout << ' ';
-		}
-		cout << dq.size();
-	}
+        if(i){
+            cout << ' ';
+        }
 
-	cout << '\n';
+        // Imprime o número de chamadas dentro da janela
+        cout << dq.size();
+    }
 
-	return 0;
+    cout << '\n';
+
+    return 0;
 }
